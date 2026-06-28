9a6b171a3a36c17acac3804fb410d0e08ee8f71c
Mishmish Dev <mishmish@mishmish.dev>
2026-06-28 01:18:40 +0100

    compose hell

--- db-migrations/000-create-keycloak-db.sh
+++ db-migrations/000-create-keycloak-db.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --no-password --dbname "$POSTGRES_DB" \
-  -c "CREATE DATABASE keycloak OWNER \"$POSTGRES_USER\""
+createdb -U "$POSTGRES_USER" keycloak
